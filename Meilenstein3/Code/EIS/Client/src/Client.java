1cad26817894fbd44dc4099833902b53c37cff2c
Dilara Güven (inf387) <dilara.gueven@smail.th-koeln.de>
2019-01-06 23:26:48 +0100

    Code Server Client hinzugefügt

---
+++ Meilenstein3/Code/EIS/Client/src/Client.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+public class Client
+{
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("192.168.2.109",8080); //"192.168.43.92"
+            System.out.println("Client started");
+
+            OutputStream out=s.getOutputStream();
+            PrintWriter writer=new PrintWriter(out);
+            InputStream in=s.getInputStream();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+            int ui=-1;
+
+            while (ui!=0)
+            {
+                System.out.println("\nMögliche Befehle: ");
+                System.out.println("[1] LED AN/AUS");
+                System.out.println("[2] Temperatur anzeigen");
+                System.out.println("[3] Stromverbrauch und Stromkosten anzeigen");
+                System.out.println("[0] Beendet den Server\n");
+                System.out.println("Ihr Eingabe: ");
+                Scanner sc=new Scanner(System.in);
+                ui=sc.nextInt();
+
+                writer.write(ui+"\n");
+                writer.flush();
+
+                String recv;
+                while((recv=reader.readLine())!=null)
+                    System.out.println(recv);
+            }
+
+            System.out.println("Closing connection to server...");
+            reader.close();
+            writer.close();
+            System.out.println("Connection closed. Good bye.");
+        }
+        catch (SocketException e)
+        {
+            System.out.println("Server closed the connection or it could not be established");
+            System.exit(-1);
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
---
+++ Meilenstein3/Code/EIS/Server/src/Server.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Server
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            ServerSocket serverSocket=new ServerSocket(8080);
+            System.out.println("Server Running");
+
+            Socket client = serverSocket.accept();
+            System.out.println("Client connected");
+            OutputStream out=client.getOutputStream();
+            PrintWriter writer=new PrintWriter(out);
+
+            InputStream in=client.getInputStream();
+            BufferedReader reader=new BufferedReader(new InputStreamReader(in));
+
+            String send;
+            int cmd=Integer.parseInt(reader.readLine());
+            System.out.println("Message From Client: "+cmd);
+
+            if(cmd==1)
+                send="Die LED ist "+ledStatus();
+            else if(cmd==2)
+                send="Es ist "+temperature()+" Grad Celsius";
+            else if(cmd==3)
+                send="Die LED ist "+energy();
+            else
+                send="Befehl konnte nicht interpretiert werden.";
+            writer.write(send+"\n");
+            writer.flush();
+
+            reader.close();
+            writer.close();
+        }
+        catch(SocketException e)
+        {
+            System.out.println("Client disconnected");
+            System.exit(1);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    private static String ledStatus()
+    {
+        String[] led={"AN","AUS"};
+        Random random = new Random();
+        int select=random.nextInt(led.length);
+        return led[select];
+    }
+
+    private static double temperature()
+    {
+        Random random = new Random();
+        return random.nextDouble();
+    }
+
+    private static String energy()
+    {
+        Random costs = new Random();
+        Random usage = new Random();
+        double kosten=costs.nextDouble();
+        double verbrauch=usage.nextDouble();
+        return "Sie haben "+verbrauch+" KWH Strom verbrauch. Bei einem Strompreis von "+kosten+" zahlen Sie "+verbrauch*kosten;
+    }
+}
